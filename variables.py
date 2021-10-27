ea89ca4df0fa62fe152fffe3da34eeb2436990a1
billypriv02 <93233775+billypriv02@users.noreply.github.com>
2021-10-27 20:05:10 +1300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+#code to lear varaiblase 
+
+# varaibles can be a string
+Greeting = ("hello world")
+
+# they can also be numbers 
+fav_num = 7
+
+# below is a constant (the name is in UPPERCASE)
+PI = 3.14
+
+num_2 = 2
